642d72e91c334d74d6f3df0dcdede3722936ada5
Wallace Martins <wmvwallace@gmail.com>
2025-05-03 18:29:03 -0300

    feat: Adiciona arquivos de configuração do Gradle e estrutura inicial do projeto Spring Boot

---
+++ src/main/java/com/github/iwmvi/knowyourfan/KnowYourFanApplication.java
@@ -0,0 +1,13 @@
+package com.github.iwmvi.knowyourfan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowYourFanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowYourFanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/iwmvi/knowyourfan/KnowYourFanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.iwmvi.knowyourfan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowYourFanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
